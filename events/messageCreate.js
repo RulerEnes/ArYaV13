f1c03a4df9d9c05862670c61db8fd4ff17c17b33
RulerEnes <88493103+RulerEnes@users.noreply.github.com>
2021-09-08 22:35:06 +0000

    Hatalar düzeltildi

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,31 +1,28 @@
-const { prefix } = require("../config.json")
+const { prefix } = require(`../config.json`);
 const cooldown = require("../utils/cooldown")
 
 module.exports = client => {
-  
-  client.on("messageCreate", message =>{
-    
-    if (message.content.startsWith(prefix) == false) return
-    
-    const args = message.content.slice(prefix.length).trim().split(/ +/)
-    const commandName = args.shift().toLowerCase()
-    
-    const command = client.commands.get(commandName)
-    if(command == undefined)return
-    
-    //Cooldown Control
-    const cooldownTime = cooldown(command, message.author.id)
-    if (cooldownTime) return message.channel.send({ content: `Lütfen Bu Komutu Tekrar Kullanmak İçin ${cooldownTime} Saniye Bekleyiniz.`})
-    
-    
-    
-    try{
-    command.execute(message)
-    } catch(e){
-      console.log(e)
-      return message.channel.send({ content: "Bu Komut Şuan Da Hatalı!" })
-    }
-    
-  })
-}
+    client.on("messageCreate", (message) => {
+        if (message.content.startsWith(prefix) == false) return;
 
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
+        const command = client.commands.get(commandName);
+        if (command == undefined) return;
+
+        //Cooldown
+        const cooldownTime = cooldown(command, message.author.id);
+        if (cooldownTime) return message.channel.send({ content: `Bekle ${cooldownTime}` })
+
+        try {
+            command.execute(message);
+        } catch (e) {
+            console.log(e);
+            return message.channel.send({
+                content: 'Bu Komut Şu Anda Hatalı Lütfen Ruler`Grave#9102 ile iletişime geçin'
+            });
+        };
+
+    })
+};
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const Discord = require("discord.js");
 const fs = require("fs")
-const { Intents } = Discord
+const { Intents, } = Discord
 const client = new Discord.Client({
-  "intents": [Discord.Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+    "intents": [Discord.Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+})
+
+
+//Command Handler
+client.commands = new Discord.Collection();
+const commands = fs.readdirSync("./commands")
+commands.forEach(commandName => {
+    const command = require(`./commands/${commandName}`)
+    client.commands.set(command.name, command);
 })
 
 //Events
@@ -13,15 +22,14 @@ const { token } = require("./config.json")
 
 
 client.once("ready", () => {
-  console.log("ArYa Bot Hazır!")
-  
-  client.user.setPresence({status: "idle", activities: [{name: "SyntaxSoftware.Net", type: "PLAYING" }]})
-  
-  //Events
-events.forEach(event =>{
-  const eventFile = require(`./events/${event}`)
-  eventFunc(client)
-})
+    console.log("ArYa Bot Hazır!")
+
+    client.user.setPresence({ status: "dnd", activities: [{ name: "SyntaxSoftware.Net", type: "PLAYING" }] })
+        //events
+    events.forEach(event => {
+        const eventFunc = require(`./events/${event}`)
+        eventFunc(client)
+    })
 })
 
 client.login(token)
