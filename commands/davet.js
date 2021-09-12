8aaddf4724b98d3fa0ff15d94845454399bef756
RulerEnes <88493103+RulerEnes@users.noreply.github.com>
2021-09-12 16:03:12 +0000

    s.davet

---
+++ commands/davet.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "name": "davet",
+  "cooldown":5,
+  execute(message) {
+    message.channel.send({content: "Merhaba! Syntax Support Sunucuya Özel Bottur (Özel Entegrasyondur) Sadece <@799012176257482753> ekleyebilir Ama ArYa Botun Davet Linkini Sana Vericem \n \nArYa Botun Davet Linki \nhttps://discord.com/oauth2/authorize?client_id=834763013248122910&scope=bot&permissions=4228906239"})
+  }
+}
---
+++ commands/sahip.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "name": "sahip",
+   cooldown:5,
+  execute(message) {
+    message.channel.send({ content: "Merhaba! Syntax Support Botun Sahibi <@799012176257482753> \nRuler`Grave#9102"})
+  }
+}
