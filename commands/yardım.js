3cec8ced2c700739b59150acca29904a8886a94f
RulerEnes <88493103+RulerEnes@users.noreply.github.com>
2021-09-12 17:53:24 +0000

    s.yardım ve s.sahip Komutları eklendi

---
+++ commands/yardım.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "name":"yardım",
+  "cooldown":5,
+  execute(message) {
+    message.channel.send({content: "Merhaba! ArYa Bot Hakkında Sorunuz Varsa <#878939864706457631> Kanalından Sorabilirsiniz Eğer Komutları Bilmiyorsanız <#884079142209998900> Kanalından Öğrenebilirsiniz. "})
+  }
+}
--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res){
-  res.write("ArYa");
+  res.write("Syntax Support");
   res.end();
 }).listen(8080);
