69df1b626dad637774bc092a5150053d3fd37ce6
RulerEnes <88493103+RulerEnes@users.noreply.github.com>
2021-09-12 21:58:53 +0000

    Oto cevap 5in yanıtı eklendi

---
+++ commands/yazılım.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "name": "yazılım",
+  "cooldown": 10,
+  execute(message) {
+    message.channel.send({content: "Merhaba! Bugün nasıl yazılımcı olabilirsiniz ondan biraz bahsedicem. Yazılımcı olmak için iyi bir üniversite ye giderseniz iyi olur, yazılım yapmak için bilgisayar zorunlu değildir başlangıç için ama bilgisayar dan daha hızlı öğrenebilirsiniz diye düşünüyorum. Onun haricinde sık sık öğrenme çabası göstermeniz gerekiyor bu illa yüz yüze olmak zorunda değil youtube üzerinden de öğrenebilirsiniz kanal önermek gerekirse \nAli Eren Altındağ \ndediklerini dikkatli bir şekilde dinlerseniz az da olsa bir bilgi katacaktır. Azim çok önemli asla pes etme yoksa öğrenemezsin. Eğer siz bir Web Sitesi yapmak istiyorsanız Html ve CSS bilmek yeterli değildir ben bir yazılımcıyım diyemezsiniz ancak yanında birde Javascript eklerseniz ancak o zaman ben bir yazılımcıyım diyebilirsinizz"})
+  }
+}
