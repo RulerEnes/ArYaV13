1490426229c1f9c7fcb325693a94981370d0b3e3
RulerEnes <88493103+RulerEnes@users.noreply.github.com>
2021-09-13 15:37:49 +0000

    s.boostavantaj komutu eklendi

---
+++ commands/boostavantaj.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "name": "boostavantaj",
+  "cooldown":5,
+  execute(message) {
+    message.channel.send({content: "Merhaba! Sunucuya eğer Takviye de bulunursan ne gibi avantajlara sahip olacaksın bunları anlatıcam \n\n 1- <@&886035124532179006> Rolüne sahip olacaksınız ve bu rol ile sunucudaki adınızı değişebilecek siniz diğer <@&878932337646247957> ve üstü roller sadece isim değişebiliyor ama boost basarak adınızı değişebilirsiniz\n\n2-Kullanıcılar bölümünde (sola doğru kaydırılınca açılan bölüm) nitro  emojisi gözükecektir bende var isterseniz bakabilirsiniz\n\n3- <@&878932337646247957>  <@&878932777783943211>  alımları olunca sizlere öncelik hakkı\n\n4- ArYa botun Premium sistemi aktifleştirilince ücretsiz premium alacaksınız \n\n İleride Bir Çok Özellik Eklenecektir."})
+  }
+}
